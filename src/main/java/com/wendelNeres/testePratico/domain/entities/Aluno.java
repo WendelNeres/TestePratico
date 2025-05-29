c32b0c4be64350b8d9a1330ad497b5fde18694f9
WendelNeres <wendelneres.dev@gmail.com>
2025-05-29 15:51:46 -0300

    entities teacher and student created

---
+++ src/main/java/com/wendelNeres/testePratico/domain/entities/Aluno.java
@@ -0,0 +1,40 @@
+package com.wendelNeres.testePratico.domain.entities;
+
+import java.util.Objects;
+
+public class Aluno extends Usuario{
+
+    private int creditos;
+
+    public Aluno(){}
+
+    public Aluno(int creditos, Long id, String nome){
+        super(id, nome);
+        this.creditos = creditos;
+
+    }
+
+    public int getCreditos() {
+        return creditos;
+    }
+
+    public void setCreditos(int creditos) {
+        this.creditos = creditos;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Aluno aluno)) return false;
+        return getCreditos() == aluno.getCreditos() &&
+                Objects.equals(getId(), aluno.getId()) &&
+                Objects.equals(getNome(), aluno.getNome());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getNome(), getCreditos());
+    }
+
+}
---
+++ src/main/java/com/wendelNeres/testePratico/domain/entities/Professor.java
@@ -0,0 +1,9 @@
+package com.wendelNeres.testePratico.domain.entities;
+
+public class Professor extends Usuario{
+
+    public Professor(Long id, String nome){
+        super(id, nome);
+    }
+
+}
