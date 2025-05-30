0bc180f7393cc169877e615cc136ad76753cb0e6
WendelNeres <wendelneres.dev@gmail.com>
2025-05-30 09:55:39 -0300

    Create EmprestarLivroUseCaseTeste and implementation Case1

---
+++ src/test/java/com/wendelNeres/testePratico/domain/services/EmprestarLivroUseCaseTest.java
@@ -0,0 +1,85 @@
+package com.wendelNeres.testePratico.domain.services;
+
+import com.wendelNeres.testePratico.domain.entities.Aluno;
+import com.wendelNeres.testePratico.domain.entities.Biblioteca;
+import com.wendelNeres.testePratico.domain.entities.Livro;
+import com.wendelNeres.testePratico.domain.entities.Usuario;
+import com.wendelNeres.testePratico.dtos.LivroDTO;
+import com.wendelNeres.testePratico.dtos.ResponseDevolucaoDTO;
+import com.wendelNeres.testePratico.dtos.UsuarioDTO;
+import com.wendelNeres.testePratico.mappers.UsuarioMapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockedStatic;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class EmprestarLivroUseCaseTest {
+
+    @Mock
+    private Biblioteca biblioteca;
+
+
+    @Autowired
+    @InjectMocks
+    private EmprestarLivroUseCase emprestarLivroUseCase;
+
+    @BeforeEach
+    void setup(){
+        MockitoAnnotations.openMocks(this);
+    }
+
+
+    @Test
+    @DisplayName("Sucesso ao emprestar o livro")
+    void emprestarLivroCase1() {
+
+        Usuario usuario = new Aluno("Maria");
+
+
+
+        Livro livro = new Livro();
+        livro.setTitulo("Matematica");
+        livro.setValorCredito(1);
+        livro.setDisponivel(true);
+
+        List<Livro> livros = List.of(livro);
+        when(biblioteca.getLivros()).thenReturn(livros);
+
+        UsuarioDTO usuarioDTO = new UsuarioDTO(usuario.getNome(), "Aluno");
+
+        try (MockedStatic<UsuarioMapper> mockedStatic = mockStatic(UsuarioMapper.class)){
+            mockedStatic.when(() -> UsuarioMapper.toEntity(usuarioDTO)).thenReturn(usuario);
+
+
+            // Act
+            LivroDTO resposta = emprestarLivroUseCase.emprestarLivro("Matematica", usuarioDTO);
+
+            // Assert
+            assertNotNull(resposta);
+            assertEquals("Matematica", resposta.titulo());
+            assertEquals("Maria", resposta.emprestadoPara());
+            assertFalse(resposta.disponivel());
+            assertEquals(9,((Aluno) usuario).getCreditos());
+        }
+
+        verify(biblioteca, times(1)).getLivros();
+
+    }
+
+    @Test
+    @DisplayName("Excessao ao emprestar o livro")
+    void emprestarLivroCase2(){
+
+
+
+        }
+}
